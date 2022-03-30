5b5e2d50673e336ae67d32e6e1a19b1a6a7fedc1
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-30 17:48:52 +0200

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>This is about us</h2>
+      <p>
+        <small>Wanna know about us , Pleade treat me Biriyani</small>
+      </p>
+    </div>
+  );
+};
+
+export default About;
