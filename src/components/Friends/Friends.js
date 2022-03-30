aba0314b625da32ced7680678a65417ed127b328
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-30 18:49:01 +0200

    Dynamic load and use navigate to route

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Friend from "../Friend/Friend";
 
 const Friends = () => {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setFriends(data));
+  }, []);
   return (
     <div>
       <h2>Hello Friends ! How are you?</h2>
-      <p>Please lend me some money</p>
+      <p>Please lend me some money .{friends.length}</p>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend}></Friend>
+      ))}
     </div>
   );
 };
