9028a4b28ba226f77cbc2e382fe8da03ae44d797
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-30 18:12:38 +0200

    Router setup and simple route

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -3,7 +3,7 @@ import React from "react";
 const Friends = () => {
   return (
     <div>
-      <h2>Hello Friends</h2>
+      <h2>Hello Friends ! How are you?</h2>
       <p>Please lend me some money</p>
     </div>
   );
