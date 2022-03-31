71aa93f2371a20054f79e60bf18a3e777445e17c
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-31 14:37:32 +0200

    Using of nested route

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const FriendDetail = () => {
   const { friendId } = useParams();
+  const [friend, setFriend] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFriend(data));
+  }, []);
 
   return (
     <div>
       <h2>This is details of friend {friendId}</h2>
+      <h3>Name: {friend.name}</h3>
+      <h4>Email: {friend.email} </h4>
+      <h5>Website: {friend.website}</h5>
+      <h6>City: {friend.address?.city} </h6>
+      <h6></h6>
     </div>
   );
 };
